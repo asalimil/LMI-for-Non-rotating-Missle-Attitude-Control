8144c3543061e4c27e52e177257c49c1efe78e43
asalimil <43479233+asalimil@users.noreply.github.com>
2019-11-23 18:10:52 -0700

    Create LMI_nonrotating_missle_attitude_control.m

---
+++ LMI_nonrotating_missle_attitude_control.m
@@ -0,0 +1,35 @@
+%% LMI for Non-rotating Missle Attitude
+clc; clear; close all;
+
+A = [-0.5 1 0 0 0
+     -62 -0.16 0 0 30
+     10 0 -50  40 5
+     0  0  0  -40 0
+     0 0   0   0 -20];
+ B1 = [0 0 
+     0 0
+     0 0
+     40 0
+     0 20];
+ B2 = [0.001 -0.05 0 0 0]';
+ C = [1.5 0 0 0 0.8
+      0 0 1 0 0 ];
+  D1 = 0; D2 = [0.01 0]';
+  
+  
+X = sdpvar(5,5);
+W = sdpvar(2,5);
+gamma = sdpvar(1);
+Const = [];
+eta = 0.00000001;
+Const = [Const, X >= eta*eye(size(X))];
+M = [(A*X+B1*W)'+(A*X+B1*W)   B2   (C*X+D1*W)'
+        B2'      -gamma*eye(1)    D2'
+        C*X+D1*W    D2      -gamma*eye(2)];
+Const = [Const, M <= -eta*eye(size(M))];
+optimize(Const, gamma);
+
+W = value(W)
+X = value(X)
+K = W*pinv(X)
+gamma = value(gamma)
